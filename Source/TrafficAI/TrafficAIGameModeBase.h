fb3836f20384d7f1d77cd56c8818425100f7053a
Anupam Sahu <sahuanupam7@gmail.com>
2023-05-06 18:19:21 +0530

    Initial commit

---
+++ Source/TrafficAI/TrafficAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TrafficAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAFFICAI_API ATrafficAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
