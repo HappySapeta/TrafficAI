72e1b172566e679fbcfd31f62320b7ba4fc4b992
Anupam Sahu <sahuanupam7@gmail.com>
2023-06-11 18:29:30 +0530

    Added AStarPathFinder submodule to plugins.

--- Source/TrafficAI/TrafficAI.Build.cs
+++ Source/TrafficAI/TrafficAI.Build.cs
@@ -8,7 +8,7 @@ public class TrafficAI : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AStarPathFinder" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
