bbee47256f67d88f96a11ba3d8c10b9bb1e526ba
Anupam Sahu <sahuanupam7@gmail.com>
2023-11-06 18:34:04 +0530

    Replaced constraint model with chaos movement.

--- Source/TrafficAI/Vehicles/TrafficAIVehicle.cpp
+++ Source/TrafficAI/Vehicles/TrafficAIVehicle.cpp
@@ -1,65 +1,38 @@
 ﻿// Copyright Anupam Sahu. All Rights Reserved.
 
 #include "TrafficAIVehicle.h"
-#include "Components/SphereComponent.h"
-#include "PhysicsEngine/PhysicsConstraintComponent.h"
+
+#include "ChaosVehicleManager.h"
+#include "ChaosWheeledVehicleMovementComponent.h"
 
 // Sets default values
 ATrafficAIVehicle::ATrafficAIVehicle()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	VehicleMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VehicleMesh"));
-	SetRootComponent(VehicleMesh);
+	VehicleRoot = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VehicleRoot"));
+	SetRootComponent(VehicleRoot);
 
-	SetupWheel("WheelFL");
-	SetupWheel("WheelFR");
-	SetupWheel("WheelRL");
-	SetupWheel("WheelRR");
+	ChaosMovement = CreateDefaultSubobject<UChaosWheeledVehicleMovementComponent>(TEXT("ChaosWheelVehicleMovement"));
+	bIsMovementComponentEnabled = true;
 }
 
-void ATrafficAIVehicle::SetupWheel(const char* Suffix)
+void ATrafficAIVehicle::SetChaosEnabled(const bool bInEnable)
 {
-	USphereComponent* WheelCollider = CreateDefaultSubobject<USphereComponent>(*FString::Printf(TEXT("Collider_%hs"), Suffix));
-	WheelCollider->SetupAttachment(VehicleMesh);
-	WheelCollider->SetSimulatePhysics(true);
-	WheelCollider->SetEnableGravity(true);
-	WheelCollider->SetCollisionObjectType(ECC_Vehicle);
-	WheelCollider->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
-	WheelCollider->SetCollisionResponseToAllChannels(ECR_Block);
-	WheelCollider->SetCollisionResponseToChannel(ECC_Vehicle, ECR_Ignore);
+	FChaosVehicleManager* VehicleManager = FChaosVehicleManager::GetVehicleManagerFromScene(GetWorld()->GetPhysicsScene());
+	check(VehicleManager);
 
-	const FString AxisConstraintName = FString::Printf(TEXT("AxisConstraint_%hs"), Suffix);
-	UPhysicsConstraintComponent* AxisConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(*AxisConstraintName);
-	AxisConstraint->SetupAttachment(VehicleMesh);
-	AxisConstraint->ComponentName1.ComponentName = *WheelCollider->GetName();
-	AxisConstraint->ComponentName2.ComponentName = *VehicleMesh->GetName();
-	AxisConstraint->SetLinearXLimit(LCM_Free, 0.0f);
-	AxisConstraint->SetLinearYLimit(LCM_Free, 0.0f);
-	AxisConstraint->SetLinearZLimit(LCM_Free, 0.0f);
-	AxisConstraint->SetAngularSwing1Limit(ACM_Locked, 0.0);
-	AxisConstraint->SetAngularSwing2Limit(ACM_Free, 0.0);
-	AxisConstraint->SetAngularTwistLimit(ACM_Locked, 0.0);
-	AxisConstraint->SetAngularDriveMode(EAngularDriveMode::TwistAndSwing);
-	AxisConstraint->SetAngularVelocityTarget({0.0f, 0.0f, 0.0f});
-	AxisConstraint->SetAngularVelocityDriveTwistAndSwing(false, true);
-	AxisConstraint->SetupAttachment(WheelCollider);
-
-	const FString SuspensionConstraintName = FString::Printf(TEXT("SuspensionConstraint_%hs"), Suffix);
-	UPhysicsConstraintComponent* SuspensionConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(*SuspensionConstraintName);
-	SuspensionConstraint->SetupAttachment(VehicleMesh);
-	SuspensionConstraint->ComponentName1.ComponentName = *WheelCollider->GetName();
-	SuspensionConstraint->ComponentName2.ComponentName = *VehicleMesh->GetName();
-	SuspensionConstraint->SetLinearXLimit(LCM_Locked, 0.0f);
-	SuspensionConstraint->SetLinearYLimit(LCM_Locked, 0.0f);
-	SuspensionConstraint->SetLinearZLimit(LCM_Limited, 10.0f);
-	SuspensionConstraint->SetAngularSwing1Limit(ACM_Free, 0.0);
-	SuspensionConstraint->SetAngularSwing2Limit(ACM_Free, 0.0);
-	SuspensionConstraint->SetAngularTwistLimit(ACM_Free, 0.0);
-	SuspensionConstraint->SetupAttachment(WheelCollider);
-	SuspensionConstraint->SetLinearDriveParams(1000.0f, 50.0f, 0.0f);
-
-	WheelColliders.Add(WheelCollider);
-	SuspensionConstraints.Add(SuspensionConstraint);
-	AxisConstraints.Add(AxisConstraint);
+	if(!bIsMovementComponentEnabled && bInEnable)
+	{
+		ChaosMovement->CreatePhysicsState();
+		ChaosMovement->ResetVehicleState();
+		VehicleRoot->SetSimulatePhysics(true);
+		bIsMovementComponentEnabled = true;
+	}
+	else if(bIsMovementComponentEnabled && !bInEnable)
+	{
+		ChaosMovement->DestroyPhysicsState();
+		VehicleRoot->SetSimulatePhysics(false);
+		bIsMovementComponentEnabled = false;
+	}
 }
--- Source/TrafficAI/Vehicles/TrafficAIVehicle.h
+++ Source/TrafficAI/Vehicles/TrafficAIVehicle.h
@@ -9,11 +9,7 @@ class USphereComponent;
 class UPhysicsConstraintComponent;
 
 /**
- * ATrafficAIVehicle class is the base class for all vehicles in the game.
- * It is responsible for creating a vehicle setup that includes four wheels attached to a skeletal mesh,
- * with each wheel connected through physics constraints.
- * When a player takes control of the vehicle it disables all physics constraints
- * and attaches a `ChaosWheelVehicleMovementComponent` to simulate the vehicle's movement and physics behavior.
+ *
  */
 UCLASS()
 class TRAFFICAI_API ATrafficAIVehicle : public APawn
@@ -25,32 +21,23 @@ public:
 	// Sets default values for this pawn's properties
 	ATrafficAIVehicle();
 
-	UPrimitiveComponent* GetRoot() const { return VehicleMesh; }
+	UFUNCTION(BlueprintCallable)
+	UPrimitiveComponent* GetRoot() const { return VehicleRoot; }
 
-private:
-	
-	/**
-	* Sets up a wheel with the specified suffix.
-	* @param Suffix A character string to identify the wheel.
-	*/
-	void SetupWheel(const char* Suffix);
+	UFUNCTION(BlueprintCallable)
+	void SetChaosEnabled(const bool bInEnable);
 
 protected:
 
 	// The skeletal mesh component representing the vehicle.
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Vehicle")
-	TObjectPtr<USkeletalMeshComponent> VehicleMesh;
+	TObjectPtr<USkeletalMeshComponent> VehicleRoot;
 	
-	// An array of sphere components representing colliders for wheels.
-	UPROPERTY(VisibleAnywhere, Category = "Wheels")
-	TArray<USphereComponent*> WheelColliders;
-	
-	// An array of physics constraint components used for suspension in the wheels.
-	UPROPERTY(VisibleAnywhere, Category = "Wheels")
-	TArray<UPhysicsConstraintComponent*> SuspensionConstraints;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Vehicle")
+	TObjectPtr<class UChaosWheeledVehicleMovementComponent> ChaosMovement;
 
-	// An array of physics constraint components used for restricting the rotation of wheels around the axle.
-	UPROPERTY(VisibleAnywhere, Category = "Wheels")
-	TArray<UPhysicsConstraintComponent*> AxisConstraints;
+private:
+
+	bool bIsMovementComponentEnabled;
 	
 };
