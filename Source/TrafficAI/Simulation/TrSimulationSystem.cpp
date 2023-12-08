7b376809e8478a3a12f9e518309952247154f6dc
Anupam Sahu <sahuanupam7@gmail.com>
2023-12-08 17:58:32 +0530

    Simulation System class

---
+++ Source/TrafficAI/Simulation/TrSimulationSystem.cpp
@@ -0,0 +1,48 @@
+﻿// Copyright Anupam Sahu. All Rights Reserved.
+
+
+#include "TrSimulationSystem.h"
+
+#include "Async/ParallelFor.h"
+
+void UTrSimulationSystem::RegisterEntites(TWeakPtr<TArray<FTrEntity>> NewEntities)
+{
+	Entities = NewEntities;
+	RegisterEntities_Internal();
+}
+
+void UTrSimulationSystem::RegisterEntities_Internal()
+{
+	if(!Entities.IsValid())
+	{
+		return;
+	}
+
+	const uint32 NumEntities = Entities.Pin()->Num();
+	ParallelFor(NumEntities, [this](const uint32 Index)
+	{
+		const AActor* Dummy = Entities.Pin()->operator[](Index).Dummy;
+		FVector NewPosition = Dummy->GetActorLocation();
+		Positions.Add(
+			{
+				static_cast<float>(NewPosition.X),
+				static_cast<float>(NewPosition.Y)
+			});
+
+		FVector NewHeading = Dummy->GetActorForwardVector();
+		Headings.Add(
+			{
+				static_cast<float>(NewHeading.X),
+				static_cast<float>(NewHeading.Y)
+			});
+
+		float NewSpeed = Dummy->GetVelocity().Length();
+		Speeds.Add(NewSpeed);
+		
+	});
+
+	for(uint32 Index = 0; Index < NumEntities; ++Index)
+	{
+		DrawDebugPoint(GetWorld(), FVector(Positions[Index].X, Positions[Index].Y, 500.0f), 10.0f, FColor::Blue, true);
+	}
+}
---
+++ Source/TrafficAI/Simulation/TrSimulationSystem.h
@@ -0,0 +1,34 @@
+﻿// Copyright Anupam Sahu. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TrCommon.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "TrSimulationSystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRAFFICAI_API UTrSimulationSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+
+	void RegisterEntites(TWeakPtr<TArray<FTrEntity>> NewEntities);
+
+private:
+
+	virtual void RegisterEntities_Internal();
+
+protected:
+
+	TWeakPtr<TArray<FTrEntity>> Entities;
+	
+	TArray<FVector2f> Positions;
+	TArray<FVector2f> Headings;
+	TArray<float> Speeds;
+	
+};
